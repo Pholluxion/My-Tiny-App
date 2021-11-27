21edaa747f8f448f7cde279bd9bbec855862941a
Pholluxion <penalozadaniel00@gmail.com>
2021-11-27 15:23:23 -0500

    Sprint 2

--- app/src/main/java/com/misiontic/mytinyapp/HomeActivity.java
+++ app/src/main/java/com/misiontic/mytinyapp/HomeActivity.java
@@ -2,7 +2,10 @@ package com.misiontic.mytinyapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -11,5 +14,15 @@ public class HomeActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        ImageView goToToDo = findViewById(R.id.goToToDo);
+        goToToDo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToToDo = new Intent(HomeActivity.this,ToDoActivity.class);
+                startActivity(goToToDo);
+            }
+        });
+
     }
 }
--- app/src/main/java/com/misiontic/mytinyapp/LoginActivity.java
+++ app/src/main/java/com/misiontic/mytinyapp/LoginActivity.java
@@ -8,6 +8,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
@@ -18,11 +19,20 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        Button btnLogin = findViewById(R.id.btnLogin);
         TextView btnGoToRegister = findViewById(R.id.btnGoToRegistro);
         btnGoToRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 goToRegister();
+            }
+        });
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToHome = new Intent(LoginActivity.this,HomeActivity.class);
+                startActivity(goToHome);
+                finish();
             }
         });
 
